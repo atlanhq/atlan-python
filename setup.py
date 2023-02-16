135c5d7c5cabeb4473df0c717a29a47e7c6df2cd
Ernest Hill <ernest@atlan.com>
2023-02-16 13:37:54 +0300

    Update ClassificationDef to include new field skip_display_name_uniqueness_check

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pyatlan",
-    version="0.0.5",
+    version="0.0.6",
     author="Atlan Pte, Ltd",
     author_email="engineering@atlan.com",
     description="Atlan Python Client",
