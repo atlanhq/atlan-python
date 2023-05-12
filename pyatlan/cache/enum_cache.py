00a391962adf21e3695e2ea49db9a1ef0c9d468c
Christopher Grote <cmgrote@users.noreply.github.com>
2023-05-12 18:37:04 +0100

    Adds cache of enums
    Signed-off-by: Christopher Grote <cmgrote@users.noreply.github.com>

---
+++ pyatlan/cache/enum_cache.py
@@ -0,0 +1,36 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2023 Atlan Pte. Ltd.
+from typing import Optional
+
+from pyatlan.model.enums import AtlanTypeCategory
+from pyatlan.model.typedef import EnumDef
+
+
+class EnumCache:
+
+    cache_by_name: dict[str, EnumDef] = dict()
+
+    @classmethod
+    def _refresh_cache(cls) -> None:
+        from pyatlan.client.atlan import AtlanClient
+
+        client = AtlanClient.get_default_client()
+        if client is None:
+            client = AtlanClient()
+        response = client.get_typedefs(type_category=AtlanTypeCategory.ENUM)
+        cls.cache_by_name = {}
+        if response is not None:
+            for enum in response.enum_defs:
+                type_name = enum.name
+                cls.cache_by_name[type_name] = enum
+
+    @classmethod
+    def get_by_name(cls, name: str) -> Optional[EnumDef]:
+        """
+        Retrieve the enumeration definition by its name.
+        """
+        if name:
+            if enum_def := cls.cache_by_name.get(name):
+                return enum_def
+            cls._refresh_cache()
+            return cls.cache_by_name.get(name)
