955738e30ec9c019338b1e93f6141c92601ab78b
Ernest Hill <ernest@atlan.com>
2023-06-01 14:45:27 +0300

    Added CustomMetadataProxy

--- pyatlan/model/custom_metadata.py
+++ pyatlan/model/custom_metadata.py
@@ -1,8 +1,6 @@
 from collections import UserDict
 from typing import Any, Optional
 
-from pydantic import BaseModel, PrivateAttr
-
 from pyatlan.cache.custom_metadata_cache import CustomMetadataCache
 
 
@@ -14,14 +12,19 @@ class CustomMetadataDict(UserDict):
     def __init__(self, name: str):
         super().__init__()
         self._name = name
+        self._modified = False
         id = CustomMetadataCache.get_id_for_name(name)
         self._names = set(CustomMetadataCache.map_attr_id_to_name[id].values())
         pass
 
+    @property
+    def modified(self):
+        return self._modified
+
     def __setitem__(self, key, value):
         if key not in self._names:
             raise KeyError(f"'{key}' is not a valid property name for {self._name}")
-
+        self._modified = True
         self.data[key] = value
 
     def __getitem__(self, key):
@@ -36,43 +39,60 @@ class CustomMetadataDict(UserDict):
             self.data[attribute_name] = None
 
 
-class Container(BaseModel):
-    _metadata: Optional[dict[str, CustomMetadataDict]] = PrivateAttr()
-    custom_metadata: Optional[dict[str, dict[str, str]]]
+class CustomMetadataProxy:
+    def __init__(self, business_attributes: Optional[dict[str, Any]]):
+        self._metadata: Optional[dict[str, CustomMetadataDict]] = None
+        self._business_attributes = business_attributes
+        self._modified = False
+        if self._business_attributes is None:
+            return
+        self._metadata = {}
+        for cm_id, cm_attributes in self._business_attributes.items():
+            cm_name = CustomMetadataCache.get_name_for_id(cm_id)
+            attribs = CustomMetadataDict(name=cm_name)
+            for attr_id, properties in cm_attributes.items():
+                attr_name = CustomMetadataCache.get_attr_name_for_id(cm_id, attr_id)
+                attribs[attr_name] = properties
+            attribs._modified = False
+            self._metadata[cm_name] = attribs
 
-    def __init__(self, **data):
-        super().__init__(**data)
-        if self.custom_metadata:
-            self._metadata = {}
-            for cm_id, cm_attributes in self.custom_metadata.items():
-                cm_name = CustomMetadataCache.get_name_for_id(cm_id)
-                attribs = CustomMetadataDict(name=cm_name)
-                for attr_id, properties in cm_attributes.items():
-                    attr_name = CustomMetadataCache.get_attr_name_for_id(cm_id, attr_id)
-                    attribs[attr_name] = properties
-                self._metadata[cm_name] = attribs
-
-    def _update_custom_metadata(self):
-        def convert(cm_name: str, properties: dict[str, Any]):
-            return {
-                CustomMetadataCache.get_attr_id_for_name(cm_name, key): value
-                for (key, value) in properties.items()
-            }
-
-        new_metadata = {
-            CustomMetadataCache.get_id_for_name(cm_name): convert(cm_name, attribs)
-            for (cm_name, attribs) in self._metadata.items()
-        }
-        self.custom_metadata = new_metadata
-
-    def json(self, *args, **kwargs) -> str:
-        self._update_custom_metadata()
-        return super().json(**kwargs)
-
-    def get_metadata(self, name: str) -> CustomMetadataDict:
+    def get_custom_metadata(self, name: str) -> CustomMetadataDict:
         if self._metadata is None:
             self._metadata = {}
         if name not in self._metadata:
             attribs = CustomMetadataDict(name=name)
             self._metadata[name] = attribs
         return self._metadata[name]
+
+    def set_custom_metadata(self, custom_metadata: CustomMetadataDict):
+        if self._metadata is None:
+            self._metadata = {}
+        self._metadata[custom_metadata._name] = custom_metadata
+        self._modified = True
+
+    @property
+    def modified(self) -> bool:
+        if self._modified:
+            return True
+        if self._metadata is None:
+            return False
+        for metadata_dict in self._metadata.values():
+            if metadata_dict.modified:
+                return True
+        return False
+
+    @property
+    def business_attributes(self) -> Optional[dict[str, Any]]:
+        def convert(cm_name: str, properties: CustomMetadataDict):
+            return {
+                CustomMetadataCache.get_attr_id_for_name(cm_name, key): value
+                for (key, value) in properties.items()
+            }
+
+        if self.modified and self._metadata is not None:
+            new_metadata = {
+                CustomMetadataCache.get_id_for_name(cm_name): convert(cm_name, attribs)
+                for (cm_name, attribs) in self._metadata.items()
+            }
+            self._business_attributes = new_metadata
+        return self._business_attributes
