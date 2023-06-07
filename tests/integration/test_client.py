98bfb969d98d1116ccb0a80ab7632ef1f44a5b8b
Ernest Hill <ernest@atlan.com>
2023-06-07 19:27:14 +0300

    Moved some tests from test_entity_model to test_client

--- tests/integration/test_client.py
+++ tests/integration/test_client.py
@@ -4,7 +4,14 @@ from typing import Callable, Generator
 import pytest
 
 from pyatlan.client.atlan import AtlanClient
-from pyatlan.model.assets import AtlasGlossaryTerm, Connection, Database
+from pyatlan.error import NotFoundError
+from pyatlan.model.assets import (
+    AtlasGlossary,
+    AtlasGlossaryTerm,
+    Connection,
+    Database,
+    Table,
+)
 from pyatlan.model.enums import AtlanConnectorType
 from tests.integration.lineage_test import create_database, delete_asset
 
@@ -186,3 +193,43 @@ def test_find_connections_by_name(client: AtlanClient):
     )
     assert len(connections) == 1
     assert connections[0].connector_name == AtlanConnectorType.SNOWFLAKE.value
+
+
+def test_get_asset_by_guid_good_guid(client: AtlanClient, glossary: AtlasGlossary):
+    glossary = client.get_asset_by_guid(glossary.guid, AtlasGlossary)
+    assert isinstance(glossary, AtlasGlossary)
+
+
+def test_get_asset_by_guid_when_table_specified_and_glossary_returned_raises_not_found_error(
+    client: AtlanClient, glossary: AtlasGlossary
+):
+    with pytest.raises(NotFoundError) as ex_info:
+        guid = glossary.guid
+        client.get_asset_by_guid(guid, Table)
+    assert (
+        f"Asset with GUID {guid} is not of the type requested: Table."
+        in ex_info.value.args[0]
+    )
+
+
+def test_get_asset_by_guid_bad_with_non_existent_guid_raises_not_found_error(
+    client: AtlanClient,
+):
+    with pytest.raises(
+        NotFoundError, match="Given instance guid 76d54dd6 is invalid/not found"
+    ):
+        client.get_asset_by_guid("76d54dd6", AtlasGlossary)
+
+
+def test_upsert_when_no_changes(client: AtlanClient, glossary: AtlasGlossary):
+    response = client.upsert(glossary)
+    assert not response.guid_assignments
+    assert not response.mutated_entities
+
+
+def test_get_by_qualified_name(client: AtlanClient, glossary: AtlasGlossary):
+    qualified_name = glossary.qualified_name
+    glossary = client.get_asset_by_qualified_name(
+        qualified_name=qualified_name, asset_type=AtlasGlossary
+    )
+    assert glossary.attributes.qualified_name == qualified_name
