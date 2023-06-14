663e16a6e04ac73dcbff48c0e31644082e61d1b8
Ernest Hill <ernest@atlan.com>
2023-06-14 13:55:19 +0300

    Add table_test

--- tests/unit/model/table_test.py
+++ tests/unit/model/table_test.py
@@ -1,17 +1,16 @@
 import pytest
 
 from pyatlan.model.assets import Table
-
-DEFAULT = "default"
-CONNECTOR_TYPE = "snowflake"
-TIME_STAMP = "1686532494/"
-DATABASE_NAME = "MyDB"
-SCHEMA_NAME = "MySchema"
-TABLE_NAME = "MyTable"
-CONNECTION_QUALIFIED_NAME = f"{DEFAULT}/{CONNECTOR_TYPE}/{TIME_STAMP}"
-DATABASE_QUALIFIED_NAME = f"{CONNECTION_QUALIFIED_NAME}/{DATABASE_NAME}"
-SCHEMA_QUALIFIED_NAME = f"{DATABASE_QUALIFIED_NAME}/{SCHEMA_NAME}"
-TABLE_QUALIFIED_NAME = f"{SCHEMA_QUALIFIED_NAME}/{TABLE_NAME}"
+from tests.unit.model.constants import (
+    CONNECTION_QUALIFIED_NAME,
+    CONNECTOR_TYPE,
+    DATABASE_NAME,
+    DATABASE_QUALIFIED_NAME,
+    SCHEMA_NAME,
+    SCHEMA_QUALIFIED_NAME,
+    TABLE_NAME,
+    TABLE_QUALIFIED_NAME,
+)
 
 
 @pytest.mark.parametrize(
@@ -33,9 +32,46 @@ def test_create():
     sut = Table.create(name=TABLE_NAME, schema_qualified_name=SCHEMA_QUALIFIED_NAME)
 
     assert sut.name == TABLE_NAME
+    assert sut.database_name == DATABASE_NAME
+    assert sut.connection_qualified_name == CONNECTION_QUALIFIED_NAME
+    assert sut.database_qualified_name == DATABASE_QUALIFIED_NAME
     assert sut.qualified_name == TABLE_QUALIFIED_NAME
-    assert sut.connector_name == CONNECTOR_TYPE
-    assert sut.sch
     assert sut.schema_qualified_name == SCHEMA_QUALIFIED_NAME
     assert sut.schema_name == SCHEMA_NAME
-    assert sut.database_qualified_name == DATABASE_QUALIFIED_NAME
+    assert sut.connector_name == CONNECTOR_TYPE
+    assert sut.atlan_schema.qualified_name == SCHEMA_QUALIFIED_NAME
+
+
+@pytest.mark.parametrize(
+    "qualified_name, name, message",
+    [
+        (None, TABLE_QUALIFIED_NAME, "qualified_name is required"),
+        (TABLE_NAME, None, "name is required"),
+    ],
+)
+def test_create_for_modification_with_invalid_parameter_raises_value_error(
+    qualified_name: str, name: str, message: str
+):
+
+    with pytest.raises(ValueError, match=message):
+        Table.create_for_modification(qualified_name=qualified_name, name=name)
+
+
+def test_create_for_modification():
+
+    sut = Table.create_for_modification(
+        qualified_name=TABLE_QUALIFIED_NAME, name=TABLE_NAME
+    )
+
+    assert sut.qualified_name == TABLE_QUALIFIED_NAME
+    assert sut.name == TABLE_NAME
+
+
+def test_trim_to_required():
+
+    sut = Table.create_for_modification(
+        qualified_name=TABLE_QUALIFIED_NAME, name=TABLE_NAME
+    ).trim_to_required()
+
+    assert sut.qualified_name == TABLE_QUALIFIED_NAME
+    assert sut.name == TABLE_NAME
