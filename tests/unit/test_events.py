5a3109a4976052672f3cc1a19c3abccdec60eb6e
Christopher Grote <cmgrote@users.noreply.github.com>
2023-06-26 15:27:22 +0100

    Adds minimal example for a signed event
    Signed-off-by: Christopher Grote <cmgrote@users.noreply.github.com>

--- tests/unit/test_events.py
+++ tests/unit/test_events.py
@@ -85,7 +85,16 @@ ACTUAL_PAYLOAD = (
     '    "time": "21/Jun/2023:16:04:21 +0000",'
     '    "timeEpoch": 1687363461544'
     "  },"
-    '  "body": "{\\"atlan-webhook\\": \\"Hello, humans of data! It worked. Excited to see what you build!\\"}",'
+    '  "body": "{\\"version\\":\\"0\\",\\"id\\":\\"256b9058-ba7a-7236-1b47-88406fdc4d2d\\",\\"detail-type\\":'
+    '\\"ENTITY_CREATE\\",\\"source\\":\\"com.atlan.kafka\\",\\"account\\":\\"123456789012\\",\\"time\\":'
+    '\\"2022-10-26T13:52:17Z\\",\\"region\\":\\"ap-south-1\\",\\"resources\\":[],\\"detail\\":{\\"source\\":{},'
+    '\\"version\\":{\\"version\\":\\"1.0.0\\",\\"versionParts\\":[1]},\\"msgCompressionKind\\":\\"NONE\\",'
+    '\\"msgSplitIdx\\":1,\\"msgSplitCount\\":1,\\"msgSourceIP\\":\\"10.121.193.228\\",\\"msgCreatedBy\\":\\"\\",'
+    '\\"msgCreationTime\\":1666792334952,\\"spooled\\":false,\\"message\\":{\\"type\\":\\"ENTITY_NOTIFICATION_V2\\",'
+    '\\"entity\\":{\\"typeName\\":\\"AtlasGlossaryTerm\\",\\"attributes\\":{\\"qualifiedName\\":'
+    '\\"rrSsMGHn1q1W2dWHqSHWe@Q07rYkHOACJkmUZJgmKFP\\",\\"name\\":\\"Example\\"},\\"guid\\":'
+    '\\"bacab52b-6c4b-4dbe-b5da-97ec2e509c7e\\",\\"status\\":\\"ACTIVE\\",\\"displayText\\":\\"Example\\"},'
+    '\\"operationType\\":\\"ENTITY_CREATE\\",\\"eventTime\\":1666792332986}}}",'
     '  "isBase64Encoded": false'
     "}"
 )
@@ -97,11 +106,11 @@ def test_validation_payload():
     assert is_validation_request(body)
 
 
-def test_signing_key():
-    event = json.loads(ACTUAL_PAYLOAD)
-    assert valid_signature(SIGNING, event.get("headers"))
-
-
 def test_no_signing_key():
     event = json.loads(VALIDATION_PAYLOAD)
     assert not valid_signature(SIGNING, event.get("headers"))
+
+
+def test_signing_key():
+    event = json.loads(ACTUAL_PAYLOAD)
+    assert valid_signature(SIGNING, event.get("headers"))
