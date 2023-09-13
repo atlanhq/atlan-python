99d0651eea56f8f716e8cd8305384967607e090f
Ernest Hill <ernest@atlan.com>
2023-09-13 09:20:33 +0300

    Refactored enum_cache

--- pyatlan/cache/enum_cache.py
+++ pyatlan/cache/enum_cache.py
@@ -3,7 +3,7 @@
 from typing import Optional
 
 from pyatlan.model.enums import AtlanTypeCategory
-from pyatlan.model.typedef import EnumDef
+from pyatlan.model.typedef import EnumDef, TypeDefResponseProvider
 
 
 class EnumCache:
@@ -12,23 +12,24 @@ class EnumCache:
     """
 
     cache_by_name: dict[str, EnumDef] = dict()
+    caches: dict[int, "EnumCache"] = {}
+
+    @classmethod
+    def get_cache(cls) -> "EnumCache":
+        from pyatlan.client.atlan import AtlanClient
+
+        client = AtlanClient.get_default_client()
+        cache_key = client.cache_key
+        if cache_key not in cls.caches:
+            cls.caches[cache_key] = EnumCache(provider=client)
+        return cls.caches[cache_key]
 
     @classmethod
     def refresh_cache(cls) -> None:
         """
         Refreshes the cache of enumerations by requesting the full set of enumerations from Atlan.
         """
-        from pyatlan.client.atlan import AtlanClient
-
-        client = AtlanClient.get_default_client()
-        if client is None:
-            client = AtlanClient()
-        response = client.get_typedefs(type_category=AtlanTypeCategory.ENUM)
-        cls.cache_by_name = {}
-        if response is not None:
-            for enum in response.enum_defs:
-                type_name = enum.name
-                cls.cache_by_name[type_name] = enum
+        cls.get_cache()._refresh_cache()
 
     @classmethod
     def get_by_name(cls, name: str) -> Optional[EnumDef]:
@@ -38,9 +39,33 @@ class EnumCache:
         :param name: human-readable name of the enumeration
         :returns: the enumeration definition
         """
+        return cls.get_cache()._get_by_name(name=name)
+
+    def __init__(self, provider: TypeDefResponseProvider):
+        self.provider = provider
+        self.cache_by_name: dict[str, EnumDef] = {}
+
+    def _refresh_cache(self) -> None:
+        """
+        Refreshes the cache of enumerations by requesting the full set of enumerations from Atlan.
+        """
+        response = self.provider.get_typedefs(type_category=AtlanTypeCategory.ENUM)
+        self.cache_by_name = {}
+        if response is not None:
+            for enum in response.enum_defs:
+                type_name = enum.name
+                self.cache_by_name[type_name] = enum
+
+    def _get_by_name(self, name: str) -> Optional[EnumDef]:
+        """
+        Retrieve the enumeration definition by its name.
+
+        :param name: human-readable name of the enumeration
+        :returns: the enumeration definition
+        """
         if name:
-            if enum_def := cls.cache_by_name.get(name):
+            if enum_def := self.cache_by_name.get(name):
                 return enum_def
-            cls.refresh_cache()
-            return cls.cache_by_name.get(name)
+            self._refresh_cache()
+            return self.cache_by_name.get(name)
         return None
