e500c3c973cae9c07cfd4424a667cab2cbc5c809
Ernest Hill <ernest@atlan.com>
2023-09-26 15:56:14 +0300

    Remove extra classes

--- pyatlan/model/aggregation.py
+++ pyatlan/model/aggregation.py
@@ -2,9 +2,7 @@
 # Copyright 2023 Atlan Pte. Ltd.
 from __future__ import annotations
 
-from typing import Any, Optional, Union
-
-from pydantic import Field
+from typing import Any, Union
 
 from pyatlan.model.core import AtlanObject
 
@@ -15,39 +13,11 @@ class AggregationMetricResult(AtlanObject):
     value: float
 
 
-class Stats(AtlanObject):
-    value: int
-    relation: str
-
-
-class Details(AtlanObject):
-    index: str = Field(alias="_index")
-    type_: str = Field(alias="_type")
-    id_: str = Field(alias="_id")
-    score: str = Field(alias="_score")
-    source: Optional[dict[str, Any]] = Field(alias="_source")
-
-
-class Hits(AtlanObject):
-    total: Stats
-    max_score: float
-    hits: list[Details]
-
-
-class AggregationHitsResult(AtlanObject):
-    """Captures the results from a bucket aggregation."""
-
-    hits: Optional[Hits]
-
-
 class AggregationBucketDetails(AtlanObject):
     """Captures the results of a single bucket within an aggregation."""
 
     key: Any
     doc_count: int
-    max_matching_length: Optional[int]
-    to: Optional[Any]
-    source_value: Optional[AggregationHitsResult]
 
 
 class AggregationBucketResult(AtlanObject):
