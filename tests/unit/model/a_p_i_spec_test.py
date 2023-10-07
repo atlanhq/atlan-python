46016fb4ad82c7b5ca6222bcb5f51c5dbb179d05
Karanjot Singh <drquark@duck.com>
2023-10-07 12:44:30 +0530

    Fix unit tests for API Assets
    Signed-off-by: Karanjot Singh <drquark@duck.com>

--- tests/unit/model/a_p_i_spec_test.py
+++ tests/unit/model/a_p_i_spec_test.py
@@ -1,13 +1,13 @@
 import pytest
 
 from pyatlan.model.assets import APISpec
-from pyatlan.model.enums import AtlanConnectorType
 from tests.unit.model.constants import (
-    API_QUALIFIED_NAME,
-    API_SPEC_NAME,
     API_CONNECTION_QUALIFIED_NAME,
     API_CONNECTOR_TYPE,
+    API_QUALIFIED_NAME,
+    API_SPEC_NAME,
 )
+
 
 @pytest.mark.parametrize(
     "name, connection_qualified_name, message",
@@ -16,14 +16,12 @@ from tests.unit.model.constants import (
         (API_SPEC_NAME, None, "connection_qualified_name is required"),
     ],
 )
-
 def test_create_with_missing_parameters_raise_value_error(
     name: str, connection_qualified_name: str, message: str
 ):
     with pytest.raises(ValueError, match=message):
-        APISpec.create(
-            name=name, connection_qualified_name=connection_qualified_name
-        )
+        APISpec.create(name=name, connection_qualified_name=connection_qualified_name)
+
 
 def test_create():
     sut = APISpec.create(
@@ -34,6 +32,7 @@ def test_create():
     assert sut.connection_qualified_name == API_CONNECTION_QUALIFIED_NAME
     assert sut.qualified_name == API_QUALIFIED_NAME
     assert sut.connector_name == API_CONNECTOR_TYPE
+
 
 @pytest.mark.parametrize(
     "qualified_name, name, message",
@@ -48,6 +47,7 @@ def test_create_for_modification_with_invalid_parameter_raises_value_error(
     with pytest.raises(ValueError, match=message):
         APISpec.create_for_modification(qualified_name=qualified_name, name=name)
 
+
 def test_create_for_modification():
     sut = APISpec.create_for_modification(
         qualified_name=API_QUALIFIED_NAME, name=API_SPEC_NAME
@@ -55,6 +55,7 @@ def test_create_for_modification():
 
     assert sut.qualified_name == API_QUALIFIED_NAME
     assert sut.name == API_SPEC_NAME
+
 
 def test_trim_to_required():
     sut = APISpec.create_for_modification(
