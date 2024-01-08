97858cd15b2ed95103ed786d7174d2bb39111500
Aryamanz29 <aryamanz29@gmail.com>
2024-01-08 17:06:30 +0530

    DVX-130: Fixes setup of the `ADLSObject` asset

--- tests/unit/model/a_d_l_s_object_test.py
+++ tests/unit/model/a_d_l_s_object_test.py
@@ -1,8 +1,8 @@
 import pytest
 
 from pyatlan.model.assets import ADLSObject
+from pyatlan.utils import get_parent_qualified_name
 from tests.unit.model.constants import (
-    ADLS_ACCOUNT_QUALIFIED_NAME,
     ADLS_CONNECTION_QUALIFIED_NAME,
     ADLS_CONNECTOR_TYPE,
     ADLS_CONTAINER_QUALIFIED_NAME,
@@ -13,34 +13,21 @@ from tests.unit.model.constants import (
 
 # Test cases for missing parameters
 @pytest.mark.parametrize(
-    "name, adls_container_qualified_name, adls_account_qualified_name, message",
+    "name, adls_container_qualified_name, message",
     [
-        (None, "adls/container", "adls_account_qualified_name", "name is required"),
-        (
-            ADLS_OBJECT_NAME,
-            None,
-            ADLS_ACCOUNT_QUALIFIED_NAME,
-            "adls_container_qualified_name is required",
-        ),
-        (
-            ADLS_OBJECT_NAME,
-            ADLS_CONTAINER_QUALIFIED_NAME,
-            None,
-            "adls_account_qualified_name is required",
-        ),
+        (None, "adls/container", "name is required"),
+        (ADLS_OBJECT_NAME, None, "adls_container_qualified_name is required"),
     ],
 )
 def test_create_with_missing_parameters_raise_value_error(
     name: str,
     adls_container_qualified_name: str,
-    adls_account_qualified_name: str,
     message: str,
 ):
     with pytest.raises(ValueError, match=message):
         ADLSObject.create(
             name=name,
             adls_container_qualified_name=adls_container_qualified_name,
-            adls_account_qualified_name=adls_account_qualified_name,
         )
 
 
@@ -49,7 +36,6 @@ def test_create():
     sut = ADLSObject.create(
         name=ADLS_OBJECT_NAME,
         adls_container_qualified_name=ADLS_CONTAINER_QUALIFIED_NAME,
-        adls_account_qualified_name=ADLS_ACCOUNT_QUALIFIED_NAME,
     )
 
     assert sut.name == ADLS_OBJECT_NAME
@@ -57,7 +43,9 @@ def test_create():
     assert sut.qualified_name == f"{ADLS_CONTAINER_QUALIFIED_NAME}/{ADLS_OBJECT_NAME}"
     assert sut.connection_qualified_name == ADLS_CONNECTION_QUALIFIED_NAME
     assert sut.connector_name == ADLS_CONNECTOR_TYPE
-    assert sut.adls_account_qualified_name == ADLS_ACCOUNT_QUALIFIED_NAME
+    assert sut.adls_account_qualified_name == get_parent_qualified_name(
+        ADLS_CONTAINER_QUALIFIED_NAME
+    )
 
 
 # Test cases for creating ADLSObject for modification
