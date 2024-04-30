8e8238f54fb0d113dd7d56f483a73b041fe108ad
Aryamanz29 <aryamanz29@gmail.com>
2024-04-30 23:14:50 +0530

    Adds support for `Snowflake Miner`

---
+++ pyatlan/model/packages/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+from .confluent_kafka_crawler import ConfluentKafkaCrawler
+from .dbt_crawler import DbtCrawler
+from .glue_crawler import GlueCrawler
+from .powerbi_crawler import PowerBICrawler
+from .snowflake_crawler import SnowflakeCrawler
+from .snowflake_miner import SnowflakeMiner
+from .tableau_crawler import TableauCrawler
