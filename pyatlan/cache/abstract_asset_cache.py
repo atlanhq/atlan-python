576142f3b253b49bc98220065866695bae947f0e
Aryamanz29 <aryamanz29@gmail.com>
2024-10-17 17:45:46 +0530

    DVX-709: Implemented `ConnectionCache`

---
+++ pyatlan/cache/abstract_asset_cache.py
@@ -0,0 +1,96 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2024 Atlan Pte. Ltd.
+
+import threading
+from abc import ABC, abstractmethod
+from typing import Any
+
+from pyatlan.errors import ErrorCode
+from pyatlan.model.assets import Asset
+from pyatlan.model.enums import AtlanConnectorType
+
+
+class AbstractAssetCache(ABC):
+
+    def __init__(self, client):
+        self.client = client
+        self.lock = threading.Lock()
+        self.name_to_guid = dict()
+        self.guid_to_asset = dict()
+        self.qualified_name_to_guid = dict()
+
+    @classmethod
+    @abstractmethod
+    def get_cache(cls):
+        """Abstract method to retreive cache."""
+
+    @abstractmethod
+    def lookup_by_guid(self, guid: str):
+        """Abstract method to lookup asset by guid."""
+
+    @abstractmethod
+    def lookup_by_qualified_name(self, qualified_name: str):
+        """Abstract method to lookup asset by qualified name."""
+
+    @abstractmethod
+    def lookup_by_name(self, name: Any):
+        """Abstract method to lookup asset by name."""
+
+    @abstractmethod
+    def get_name(self, asset: Asset):
+        """Abstract method to get name from asset."""
+
+    def is_guid_known(self, guid: str):
+        return guid in self.guid_to_asset
+
+    def is_qualified_name_known(self, qualified_name: str):
+        return qualified_name in self.qualified_name_to_guid
+
+    def is_name_known(self, name: str):
+        return name in self.name_to_guid
+
+    def cache(self, asset: Asset):
+        name = asset and self.get_name(asset)
+        if not all([name, asset.guid, asset.qualified_name]):
+            return
+        self.name_to_guid[name] = asset.guid
+        self.guid_to_asset[asset.guid] = asset
+        self.qualified_name_to_guid[asset.qualified_name] = asset.guid
+
+    def _get_by_guid(self, guid: str, allow_refresh: bool = True):
+        if not guid:
+            raise ErrorCode.MISSING_ID.exception_with_parameters()
+        asset = self.guid_to_asset.get(guid)
+        if not asset and allow_refresh:
+            self.lookup_by_guid(guid)
+            asset = self.guid_to_asset.get(guid)
+        if not asset:
+            raise ErrorCode.ASSET_NOT_FOUND_BY_GUID.exception_with_parameters(guid)
+        return asset
+
+    def _get_by_qualified_name(self, qualified_name: str, allow_refresh: bool = True):
+        if not qualified_name:
+            raise ErrorCode.MISSING_ID.exception_with_parameters()
+        guid = self.qualified_name_to_guid.get(qualified_name)
+        if not guid and allow_refresh:
+            self.lookup_by_qualified_name(qualified_name)
+            guid = self.qualified_name_to_guid.get(qualified_name)
+        if not guid:
+            raise ErrorCode.ASSET_NOT_FOUND_BY_QN.exception_with_parameters(
+                qualified_name,
+                AtlanConnectorType.get_connector_name(qualified_name),
+            )
+        return self._get_by_guid(guid=guid, allow_refresh=False)
+
+    def _get_by_name(self, name: Any, allow_refresh: bool = True):
+        if not name:
+            raise ErrorCode.MISSING_NAME.exception_with_parameters()
+        guid = self.name_to_guid.get(str(name))
+        if not guid and allow_refresh:
+            self.lookup_by_name(name)
+            guid = self.name_to_guid.get(str(name))
+        if not guid:
+            raise ErrorCode.ASSET_NOT_FOUND_BY_NAME.exception_with_parameters(
+                name._TYPE_NAME, name
+            )
+        return self._get_by_guid(guid=guid, allow_refresh=False)
---
+++ pyatlan/cache/connection_cache.py
@@ -0,0 +1,146 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2024 Atlan Pte. Ltd.
+from __future__ import annotations
+
+import logging
+import threading
+from typing import Dict, Optional, Union
+
+from pyatlan.cache.abstract_asset_cache import AbstractAssetCache
+from pyatlan.client.atlan import AtlanClient
+from pyatlan.model.assets import Asset, Connection
+from pyatlan.model.enums import AtlanConnectorType
+from pyatlan.model.fluent_search import FluentSearch
+
+LOGGER = logging.getLogger(__name__)
+
+lock = threading.Lock()
+
+
+class ConnectionCache(AbstractAssetCache):
+
+    _SEARCH_FIELDS = [
+        Connection.NAME,
+        Connection.STATUS,
+        Connection.CONNECTOR_NAME,
+    ]
+    SEARCH_ATTRIBUTES = [field.atlan_field_name for field in _SEARCH_FIELDS]
+    caches: Dict[int, ConnectionCache] = dict()
+
+    def __init__(self, client: AtlanClient):
+        super().__init__(client)
+
+    @classmethod
+    def get_cache(cls) -> ConnectionCache:
+        from pyatlan.client.atlan import AtlanClient
+
+        with lock:
+            default_client = AtlanClient.get_default_client()
+            cache_key = default_client.cache_key
+            if cache_key not in cls.caches:
+                cls.caches[cache_key] = ConnectionCache(client=default_client)
+            return cls.caches[cache_key]
+
+    @classmethod
+    def get_by_guid(cls, guid: str, allow_refresh: bool = True) -> Connection:
+        return cls.get_cache()._get_by_guid(guid=guid, allow_refresh=allow_refresh)
+
+    @classmethod
+    def get_by_qualified_name(
+        cls, qualified_name: str, allow_refresh: bool = True
+    ) -> Connection:
+        return cls.get_cache()._get_by_qualified_name(
+            qualified_name=qualified_name, allow_refresh=allow_refresh
+        )
+
+    @classmethod
+    def get_by_name(cls, name: str, allow_refresh: bool = True) -> Connection:
+        return cls.get_cache()._get_by_name(name=name, allow_refresh=allow_refresh)
+
+    def lookup_by_guid(self, guid: Optional[str]) -> None:
+        if not guid:
+            return
+        with self.lock:
+            response = (
+                FluentSearch(_includes_on_results=self.SEARCH_ATTRIBUTES)
+                .select()
+                .where(Connection.GUID.eq(guid))
+                .execute(self.client)
+            )
+            candidate = (response.current_page() and response.current_page()[0]) or None
+            if candidate and isinstance(candidate, Connection):
+                self.cache(candidate)
+
+    def lookup_by_qualified_name(self, connection_qn: Optional[str]) -> None:
+        if not connection_qn:
+            return
+        with self.lock:
+            response = (
+                FluentSearch(_includes_on_results=self.SEARCH_ATTRIBUTES)
+                .select()
+                .where(Connection.QUALIFIED_NAME.eq(connection_qn))
+                .execute(self.client)
+            )
+            candidate = (response.current_page() and response.current_page()[0]) or None
+            if candidate and isinstance(candidate, Connection):
+                self.cache(candidate)
+
+    def lookup_by_name(self, name: ConnectionName) -> None:
+        if not isinstance(name, ConnectionName):
+            return
+        results = self.client.asset.find_connections_by_name(
+            name=name.name,
+            connector_type=name.type,
+            attributes=self.SEARCH_ATTRIBUTES,
+        )
+        if not results:
+            return
+        if len(results) > 1:
+            LOGGER.warning(
+                (
+                    "Found multiple connections of the same type "
+                    "with the same name, caching only the first: %s"
+                ),
+                name,
+            )
+        self.cache(results[0])
+
+    def get_name(self, asset: Asset):
+        if not isinstance(asset, Connection):
+            return
+        return str(ConnectionName(asset))
+
+
+class ConnectionName:
+    _TYPE_NAME = "Connection"
+
+    def __init__(
+        self,
+        connection: Union[
+            str,
+            Optional[Connection],
+        ] = None,
+    ):
+        self.name = None
+        self.type = None
+
+        if isinstance(connection, Connection):
+            self.name = connection.name
+            self.type = connection.connector_name
+
+        elif isinstance(connection, str):
+            tokens = connection.split("/")
+            if len(tokens) > 1:
+                self.type = AtlanConnectorType(tokens[0])  # type: ignore[call-arg]
+                self.name = connection[len(tokens[0]) + 1 :]  # noqa
+
+    def __hash__(self):
+        return hash((self.name, self.type))
+
+    def __str__(self):
+        return f"{self.type}/{self.name}"
+
+    def __eq__(self, other):
+        if isinstance(other, ConnectionName):
+            return self.name == other.name and self.type == other.type
+        return False
