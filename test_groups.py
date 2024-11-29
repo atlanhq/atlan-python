8b60e28dc266be0b37591aefd23f883f7d7c1c8e
Vaibhav Chopra <vaibhav.chopra@atlan.com>
2024-11-29 17:01:49 +0530

    Delete test_groups.py

--- test_groups.py
+++
@@ -1,8 +0,0 @@
-from pyatlan.client.atlan import AtlanClient
-import logging
-logging. basicConfig (level=logging.DEBUG) #
-client = AtlanClient()
-
-groups3 =  client.group.get_all(limit=3)
-
-print(groups3)
