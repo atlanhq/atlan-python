1eea5d98daf915adbcfb229f37c255e75e6ef3b0
vaibhavatlan <vaibhav.chopra@atlan.com>
2024-12-26 22:58:00 +0530

    Corrected the Unit tests and integration tests

--- tests/unit/test_audit_search.py
+++ tests/unit/test_audit_search.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from json import load
 from pathlib import Path
 from unittest.mock import Mock, patch
@@ -36,14 +37,23 @@ def audit_search_paging_json():
 
 
 def _assert_audit_search_results(results, response_json, sorts, bulk=False):
-    for i, result in enumerate(results.current_page()):
-        assert result
-        assert result.entity_id == response_json["entityAudits"][i]["entity_id"]
+    for audit in results:
+        assert audit.entity_id == response_json["entityAudits"][0]["entity_id"]
         assert (
-            result.entity_qualified_name
-            == response_json["entityAudits"][i]["entity_qualified_name"]
+            audit.entity_qualified_name
+            == response_json["entityAudits"][0]["entity_qualified_name"]
         )
-        assert result.type_name == response_json["entityAudits"][i]["type_name"]
+        assert audit.type_name == response_json["entityAudits"][0]["type_name"]
+        expected_timestamp = datetime.fromtimestamp(
+            response_json["entityAudits"][0]["timestamp"] / 1000, tz=timezone.utc
+        )
+        assert audit.timestamp == expected_timestamp
+        expected_created = datetime.fromtimestamp(
+            response_json["entityAudits"][0]["created"] / 1000, tz=timezone.utc
+        )
+        assert audit.created == expected_created
+        assert audit.user == response_json["entityAudits"][0]["user"]
+        assert audit.action == response_json["entityAudits"][0]["action"]
 
     assert results.total_count == response_json["totalCount"]
     assert results._bulk == bulk
@@ -65,7 +75,7 @@ def test_audit_search_pagination(
     dsl = DSL(
         query=Bool(filter=[Term(field="entityId", value="some-guid")]),
         sort=[],
-        size=2,
+        size=1,
         from_=0,
     )
     audit_search_request = AuditSearchRequest(dsl=dsl)
@@ -73,10 +83,16 @@ def test_audit_search_pagination(
     expected_sorts = [SortItem(field="entityId", order=SortOrder.ASCENDING)]
 
     _assert_audit_search_results(response, audit_search_paging_json, expected_sorts)
-    assert mock_api_caller._call_api.call_count == 1
+    assert mock_api_caller._call_api.call_count == 3
+    assert mock_logger.call_count == 0
     mock_api_caller.reset_mock()
 
     # Test bulk pagination
+    mock_api_caller._call_api.side_effect = [
+        audit_search_paging_json,
+        audit_search_paging_json,
+        {},
+    ]
     audit_search_request = AuditSearchRequest(dsl=dsl)
     response = client.search(criteria=audit_search_request, bulk=True)
     expected_sorts = [
@@ -87,6 +103,14 @@ def test_audit_search_pagination(
     _assert_audit_search_results(
         response, audit_search_paging_json, expected_sorts, bulk=True
     )
+    # The call count will be 2 because
+    # audit search entries are processed in the first API call.
+    # In the second API call, self._entity_audits
+    # becomes 0, which breaks the pagination.
+    # This differs from offset-based pagination
+    # where an additional API call is needed
+    # to verify if the results are empty
+    assert mock_api_caller._call_api.call_count == 2
     assert mock_logger.call_count == 1
     assert "Audit bulk search option is enabled." in mock_logger.call_args_list[0][0][0]
     mock_logger.reset_mock()
@@ -95,6 +119,9 @@ def test_audit_search_pagination(
     # Test automatic bulk search conversion when exceeding threshold
     with patch.object(AuditSearchResults, "_MASS_EXTRACT_THRESHOLD", -1):
         mock_api_caller._call_api.side_effect = [
+            # Extra call to re-fetch the first page
+            # results with updated timestamp sorting
+            audit_search_paging_json,
             audit_search_paging_json,
             audit_search_paging_json,
             {},
@@ -105,6 +132,7 @@ def test_audit_search_pagination(
             response, audit_search_paging_json, expected_sorts, bulk=False
         )
         assert mock_logger.call_count == 1
+        assert mock_api_caller._call_api.call_count == 3
         assert (
             "Result size (%s) exceeds threshold (%s)"
             in mock_logger.call_args_list[0][0][0]
