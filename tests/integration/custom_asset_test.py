e2a015692b9e21c8646c2a908967d652962db8bd
rupali-atlan <rupali.kishore@atlan.com>
2025-01-13 12:34:15 +0530

    added update, retrieve integration tests for custom assets

--- tests/integration/custom_asset_test.py
+++ tests/integration/custom_asset_test.py
@@ -1,11 +1,14 @@
 from typing import Generator
 
 import pytest
+from pyatlan.model.core import Announcement
 
 from pyatlan.client.atlan import AtlanClient
 from pyatlan.model.assets import Connection, CustomEntity
 from pyatlan.model.enums import (
+    AnnouncementType,
     AtlanConnectorType,
+    CertificateStatus,
     EntityStatus,
 )
 from pyatlan.model.response import AssetMutationResponse
@@ -17,6 +20,12 @@ MODULE_NAME = TestId.make_unique("CUSTOM")
 
 CONNECTOR_TYPE = AtlanConnectorType.CUSTOM
 CUSTOM_ENTITY_NAME = f"{MODULE_NAME}-custom-entity"
+
+CERTIFICATE_STATUS = CertificateStatus.VERIFIED
+ANNOUNCEMENT_TITLE = "Python SDK testing."
+ANNOUNCEMENT_TYPE = AnnouncementType.INFORMATION
+CERTIFICATE_MESSAGE = "Automated testing of the Python SDK."
+ANNOUNCEMENT_MESSAGE = "Automated testing of the Python SDK."
 
 response = block(AtlanClient(), AssetMutationResponse())
 
@@ -53,7 +62,6 @@ def test_custom_entity(
     assert custom_entity.name == CUSTOM_ENTITY_NAME
     assert custom_entity.connection_qualified_name == connection.qualified_name
     assert custom_entity.connector_name == AtlanConnectorType.CUSTOM.value
-
 
 @pytest.mark.order(after="test_custom_entity")
 def test_delete_custom_entity(
@@ -92,3 +100,59 @@ def test_restore_custom_entity(
     assert restored.guid == custom_entity.guid
     assert restored.qualified_name == custom_entity.qualified_name
     assert restored.status == EntityStatus.ACTIVE
+
+def _update_cert_and_annoucement(client, asset, asset_type):
+    assert asset.name
+    assert asset.qualified_name
+
+    updated = client.asset.update_certificate(
+        name=asset.name,
+        asset_type=asset_type,
+        qualified_name=asset.qualified_name,
+        message=CERTIFICATE_MESSAGE,
+        certificate_status=CERTIFICATE_STATUS,
+    )
+    assert updated
+    assert updated.certificate_status == CERTIFICATE_STATUS
+    assert updated.certificate_status_message == CERTIFICATE_MESSAGE
+
+    updated = client.asset.update_announcement(
+        name=asset.name,
+        asset_type=asset_type,
+        qualified_name=asset.qualified_name,
+        announcement=Announcement(
+            announcement_type=ANNOUNCEMENT_TYPE,
+            announcement_title=ANNOUNCEMENT_TITLE,
+            announcement_message=ANNOUNCEMENT_MESSAGE,
+        ),
+    )
+    assert updated
+    assert updated.announcement_type == ANNOUNCEMENT_TYPE
+    assert updated.announcement_title == ANNOUNCEMENT_TITLE
+    assert updated.announcement_message == ANNOUNCEMENT_MESSAGE
+
+def test_update_custom_assets(
+    client: AtlanClient,
+    custom_entity: CustomEntity,
+):
+    _update_cert_and_annoucement(client, custom_entity, CustomEntity)
+
+def _retrieve_custom_assets(client, asset, asset_type):
+    retrieved = client.asset.get_by_guid(
+        asset.guid, asset_type=asset_type, ignore_relationships=False
+    )
+    assert retrieved
+    assert not retrieved.is_incomplete
+    assert retrieved.guid == asset.guid
+    assert retrieved.qualified_name == asset.qualified_name
+    assert retrieved.name == asset.name
+    assert retrieved.connector_name == AtlanConnectorType.DATAVERSE
+    assert retrieved.certificate_status == CERTIFICATE_STATUS
+    assert retrieved.certificate_status_message == CERTIFICATE_MESSAGE
+
+@pytest.mark.order(after="test_update_custom_assets")
+def test_retrieve_custom_assets(
+    client: AtlanClient,
+    custom_entity: CustomEntity,
+):
+    _retrieve_custom_assets(client, custom_entity, CustomEntity)
