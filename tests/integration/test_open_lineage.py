8ec08c6c353a06b14817ea699694ca792e577320
Aryamanz29 <aryamanz29@gmail.com>
2025-01-17 16:23:20 +0530

    [test] Improved assertions for Spark job related attributes

--- tests/integration/test_open_lineage.py
+++ tests/integration/test_open_lineage.py
@@ -4,14 +4,15 @@ import pytest
 
 from pyatlan.cache.role_cache import RoleCache
 from pyatlan.client.atlan import AtlanClient
-from pyatlan.model.assets import Connection, SparkJob
+from pyatlan.model.assets import Asset, Connection, Process, SparkJob
+from pyatlan.model.audit import AuditSearchRequest
 from pyatlan.model.enums import OpenLineageEventType
 from pyatlan.model.open_lineage.event import OpenLineageEvent
 from pyatlan.model.open_lineage.job import OpenLineageJob
 from pyatlan.model.open_lineage.run import OpenLineageRun
 from tests.integration.client import TestId, delete_asset
 
-CONNECTION_NAME = TestId.make_unique("OpenLineage")
+MODULE_NAME = TestId.make_unique("OL")
 
 
 @pytest.fixture(scope="module")
@@ -19,7 +20,7 @@ def connection(client: AtlanClient):
     admin_role_guid = str(RoleCache.get_id_for_name("$admin"))
 
     c = client.open_lineage.create_connection(
-        name=CONNECTION_NAME, admin_roles=[admin_role_guid]
+        name=MODULE_NAME, admin_roles=[admin_role_guid]
     )
     guid = c.guid
     yield c
@@ -29,18 +30,16 @@ def connection(client: AtlanClient):
 def test_open_lineage_integration(connection: Connection, client: AtlanClient):
 
     assert connection is not None
-    assert connection.name == CONNECTION_NAME
+    assert connection.name == MODULE_NAME
+
     namespace = "snowflake://abc123.snowflakecomputing.com"
     producer = "https://your.orchestrator/unique/id/123"
-
     job = OpenLineageJob.creator(
-        connection_name=CONNECTION_NAME, job_name="dag_123", producer=producer
+        connection_name=MODULE_NAME, job_name="dag_123", producer=producer
     )
     run = OpenLineageRun.creator(job=job)
-
     id = job.create_input(namespace=namespace, asset_name="OPS.DEFAULT.RUN_STATS")
     od = job.create_output(namespace=namespace, asset_name="OPS.DEFAULT.FULL_STATS")
-
     od.to_fields = [
         {
             "COLUMN": [
@@ -55,7 +54,6 @@ def test_open_lineage_integration(connection: Connection, client: AtlanClient):
             ]
         },
     ]
-
     start = OpenLineageEvent.creator(run=run, event_type=OpenLineageEventType.START)
     start.inputs = [
         id,
@@ -66,43 +64,65 @@ def test_open_lineage_integration(connection: Connection, client: AtlanClient):
         od,
         job.create_output(namespace=namespace, asset_name="AN.OTHER.VIEW"),
     ]
-
     start.emit()
 
     complete = OpenLineageEvent.creator(
         run=run, event_type=OpenLineageEventType.COMPLETE
     )
     complete.emit()
+
     assert job
     assert start.event_type == OpenLineageEventType.START
     assert complete.event_type == OpenLineageEventType.COMPLETE
 
     # Awaiting the creation and storage of the Job asset in the backend
-    time.sleep(15)
+    time.sleep(30)
 
     job_qualified_name = f"{connection.qualified_name}/{job.name}"
-    job_asset = client.asset.get_by_qualified_name(
-        qualified_name=job_qualified_name,
-        asset_type=SparkJob,
-        ignore_relationships=False,
+
+    # Use the audit search, similar to UI calls,
+    # to retrieve complete information (process, inputs, outputs) about Spark jobs
+    results = client.audit.search(
+        AuditSearchRequest.by_qualified_name(
+            type_name=SparkJob.__name__,
+            qualified_name=job_qualified_name,
+        )
     )
+    assert results and results.current_page() and len(results.current_page()) > 0
+    job_asset = results.current_page()[0]
+    assert (
+        job_asset
+        and job_asset.detail
+        and isinstance(job_asset.detail, Asset)
+        and job_asset.detail.relationship_attributes
+    )
+    assert job_asset.detail.name == job.name
+    assert job_asset.detail.qualified_name == job_qualified_name
 
-    assert job_asset
-    assert job_asset.name == job.name
-
-    inputs = job_asset.inputs
-    outputs = job_asset.outputs
-    process = job_asset.process
+    inputs = job_asset.detail.relationship_attributes.get("inputs")
+    outputs = job_asset.detail.relationship_attributes.get("outputs")
+    process = job_asset.detail.relationship_attributes.get("process")
 
     assert inputs
     assert outputs
     assert process
 
-    input_display_text = [inp.display_text for inp in inputs]
-    assert input_display_text[0] == "RUN_STATS"
-    assert input_display_text[1] == "TBL"
-    assert input_display_text[2] == "TBL"
+    input_qns = {
+        input.get("uniqueAttributes", {}).get("qualifiedName") for input in inputs
+    }
+    assert f"{connection.qualified_name}/OPS/DEFAULT/RUN_STATS" in input_qns
+    assert f"{connection.qualified_name}/SOME/OTHER/TBL" in input_qns
+    assert f"{connection.qualified_name}/AN/OTHER/TBL" in input_qns
 
-    assert process.display_text == "dag_123"
+    outputs_qns = {
+        output.get("uniqueAttributes", {}).get("qualifiedName") for output in outputs
+    }
+    assert f"{connection.qualified_name}/OPS/DEFAULT/FULL_STATS" in outputs_qns
+    assert f"{connection.qualified_name}/AN/OTHER/VIEW" in outputs_qns
+    assert (
+        process.get("uniqueAttributes", {}).get("qualifiedName")
+        == f"{connection.qualified_name}/dag_123/process"
+    )
 
-    client.asset.purge_by_guid(guid=job_asset.guid)
+    delete_asset(client, asset_type=Process, guid=process.get("guid"))
+    delete_asset(client, asset_type=SparkJob, guid=job_asset.detail.guid)
