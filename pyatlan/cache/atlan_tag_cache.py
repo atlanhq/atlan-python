cb15ade46737726217bf03c11fa180c5e8818511
Aryamanz29 <aryamanz29@gmail.com>
2025-03-13 15:41:25 +0530

    APP-5519: Refactored `AtlanTagCache` to maintain a single cache store per client

--- pyatlan/cache/atlan_tag_cache.py
+++ pyatlan/cache/atlan_tag_cache.py
@@ -1,75 +1,33 @@
 # SPDX-License-Identifier: Apache-2.0
-# Copyright 2022 Atlan Pte. Ltd.
-from threading import Lock
-from typing import Dict, Optional, Set
+# Copyright 2025 Atlan Pte. Ltd
+from __future__ import annotations
 
-from pyatlan.client.typedef import TypeDefClient
+from threading import Lock
+from typing import TYPE_CHECKING, Dict, Optional, Set
+
+from pydantic.v1 import validate_arguments
+
+from pyatlan.client.common import ApiCaller
 from pyatlan.errors import ErrorCode
 from pyatlan.model.enums import AtlanTypeCategory
 from pyatlan.model.typedef import AtlanTagDef
 
-lock: Lock = Lock()
+if TYPE_CHECKING:
+    from pyatlan.client.atlan import AtlanClient
 
 
 class AtlanTagCache:
     """
-    Lazily-loaded cache for translating between Atlan-internal ID strings and human-readable names
-    for Atlan tags.
+    Lazily-loaded cache for translating between Atlan-internal ID
+    strings and human-readable names for Atlan tags.
     """
 
-    caches: Dict[int, "AtlanTagCache"] = {}
-
-    @classmethod
-    def get_cache(cls) -> "AtlanTagCache":
-        from pyatlan.client.atlan import AtlanClient
-
-        with lock:
-            client = AtlanClient.get_default_client()
-            cache_key = client.cache_key
-            if cache_key not in cls.caches:
-                cls.caches[cache_key] = AtlanTagCache(typedef_client=client.typedef)
-            return cls.caches[cache_key]
-
-    @classmethod
-    def refresh_cache(cls) -> None:
-        """
-        Refreshes the cache of Atlan tags by requesting the full set of Atlan tags from Atlan.
-        """
-        cls.get_cache()._refresh_cache()
-
-    @classmethod
-    def get_id_for_name(cls, name: str) -> Optional[str]:
-        """
-        Translate the provided human-readable Atlan tag name to its Atlan-internal ID string.
-
-        :param name: human-readable name of the Atlan tag
-        :returns: Atlan-internal ID string of the Atlan tag
-        """
-        return cls.get_cache()._get_id_for_name(name=name)
-
-    @classmethod
-    def get_name_for_id(cls, idstr: str) -> Optional[str]:
-        """
-        Translate the provided Atlan-internal classification ID string to the human-readable Atlan tag name.
-
-        :param idstr: Atlan-internal ID string of the Atlan tag
-        :returns: human-readable name of the Atlan tag
-        """
-        return cls.get_cache()._get_name_for_id(idstr=idstr)
-
-    @classmethod
-    def get_source_tags_attr_id(cls, id: str) -> Optional[str]:
-        """
-        Translate the provided Atlan-internal Atlan tag ID string to the Atlan-internal name of the attribute that
-        captures tag attachment details (for source-synced tags).
-
-        :param id: Atlan-internal ID string of the Atlan tag
-        :returns: Atlan-internal ID string of the attribute containing source-synced tag attachment details
-        """
-        return cls.get_cache()._get_source_tags_attr_id(id)
-
-    def __init__(self, typedef_client: TypeDefClient):
-        self.typdef_client: TypeDefClient = typedef_client
+    def __init__(self, client: AtlanClient):
+        if not isinstance(client, ApiCaller):
+            raise ErrorCode.INVALID_PARAMETER_TYPE.exception_with_parameters(
+                "client", "ApiCaller"
+            )
+        self.client: AtlanClient = client
         self.cache_by_id: Dict[str, AtlanTagDef] = {}
         self.map_id_to_name: Dict[str, str] = {}
         self.map_name_to_id: Dict[str, str] = {}
@@ -80,10 +38,11 @@ class AtlanTagCache:
 
     def _refresh_cache(self) -> None:
         """
-        Refreshes the cache of Atlan tags by requesting the full set of Atlan tags from Atlan.
+        Refreshes the cache of Atlan tags
+        by requesting the full set of Atlan tags from Atlan.
         """
         with self.lock:
-            response = self.typdef_client.get(
+            response = self.client.typedef.get(
                 type_category=[
                     AtlanTypeCategory.CLASSIFICATION,
                     AtlanTypeCategory.STRUCT,
@@ -107,7 +66,8 @@ class AtlanTagCache:
                             sourceTagsId = attr_def.name or ""
                     self.map_id_to_source_tags_attr_id[atlan_tag_id] = sourceTagsId
 
-    def _get_id_for_name(self, name: str) -> Optional[str]:
+    @validate_arguments
+    def get_id_for_name(self, name: str) -> Optional[str]:
         """
         Translate the provided human-readable Atlan tag name to its Atlan-internal ID string.
 
@@ -126,7 +86,8 @@ class AtlanTagCache:
                 self.deleted_names.add(name)
         return cls_id
 
-    def _get_name_for_id(self, idstr: str) -> Optional[str]:
+    @validate_arguments
+    def get_name_for_id(self, idstr: str) -> Optional[str]:
         """
         Translate the provided Atlan-internal classification ID string to the human-readable Atlan tag name.
 
@@ -145,21 +106,26 @@ class AtlanTagCache:
                 self.deleted_ids.add(idstr)
         return cls_name
 
-    def _get_source_tags_attr_id(self, id: str) -> Optional[str]:
+    @validate_arguments
+    def get_source_tags_attr_id(self, id: str) -> Optional[str]:
         """
         Translate the provided Atlan-internal Atlan tag ID string to the Atlan-internal name of the attribute that
         captures tag attachment details (for source-synced tags).
 
         :param id: Atlan-internal ID string of the Atlan tag
-        :returns: Atlan-internal ID string of the attribute containing source-synced tag attachment details
+        :returns: Atlan-internal ID string of the attribute
+        containing source-synced tag attachment details
         """
-        if id and id.strip():
-            attr_id = self.map_id_to_source_tags_attr_id.get(id)
-            if attr_id is not None or id in self.deleted_ids:
-                return attr_id
-            self.refresh_cache()
-            if attr_id := self.map_id_to_source_tags_attr_id.get(id):
-                return attr_id
-            self.deleted_ids.add(id)
-            raise ErrorCode.ATLAN_TAG_NOT_FOUND_BY_ID.exception_with_parameters(id)
-        raise ErrorCode.MISSING_ATLAN_TAG_ID.exception_with_parameters()
+        if not id or not id.strip():
+            raise ErrorCode.MISSING_ATLAN_TAG_ID.exception_with_parameters()
+
+        attr_id = self.map_id_to_source_tags_attr_id.get(id)
+        if attr_id is not None or id in self.deleted_ids:
+            return attr_id
+
+        self._refresh_cache()
+        if attr_id := self.map_id_to_source_tags_attr_id.get(id):
+            return attr_id
+
+        self.deleted_ids.add(id)
+        raise ErrorCode.ATLAN_TAG_NOT_FOUND_BY_ID.exception_with_parameters(id)
