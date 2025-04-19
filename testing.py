b23c3f25e1dd2326d60e1af65d2f97b8d08f66d6
Vaibhav Chopra <vaibhav.chopra@atlan.com>
2025-04-19 18:20:56 +0530

    Delete testing.py

--- testing.py
+++
@@ -1,8 +0,0 @@
-from pyatlan.client.atlan import AtlanClient
-from pyatlan.model.enums import AtlanWorkflowPhase
-
-client = AtlanClient()
-runs_status = client.workflow.find_by_status_and_interval(
-    [AtlanWorkflowPhase.RUNNING], 1
-)
-print(runs_status)
