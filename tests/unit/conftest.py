a13768f16d38a29f07416bf40407c6f2cde3b3a5
Aryamanz29 <aryamanz29@gmail.com>
2025-04-28 19:21:26 +0530

    [test/fix] Implemented a fix for `VCRHTTPResponse.version_string`
    https://github.com/kevin1024/vcrpy/issues/888

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -31,3 +31,19 @@ def mock_custom_metadata_cache():
 def mock_tag_cache():
     with patch("pyatlan.cache.atlan_tag_cache.AtlanTagCache") as cache:
         yield cache
+
+
+@pytest.fixture(autouse=True)
+def patch_vcr_http_response_version_string():
+    """
+    Patch the VCRHTTPResponse class to add a version_string attribute if it doesn't exist.
+
+    This patch is necessary to avoid bumping vcrpy to 7.0.0,
+    which drops support for Python 3.8.
+    """
+    from vcr.stubs import VCRHTTPResponse  # type: ignore[import-untyped]
+
+    if not hasattr(VCRHTTPResponse, "version_string"):
+        VCRHTTPResponse.version_string = None
+
+    yield
