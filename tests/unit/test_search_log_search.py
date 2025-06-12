20af73734e7daebdc41c864b9b3a01aa2402e16f
Aryamanz29 <aryamanz29@gmail.com>
2025-06-12 13:47:36 +0530

    [test] Fixed test_search_log_search mock assertions

--- tests/unit/test_search_log_search.py
+++ tests/unit/test_search_log_search.py
@@ -56,7 +56,7 @@ def _assert_search_log_results(results, response_json, sorts, bulk=False):
 @patch.object(LOGGER, "debug")
 def test_search_log_pagination(mock_logger, mock_api_caller, search_logs_json):
     client = SearchLogClient(mock_api_caller)
-    mock_api_caller._call_api.side_effect = [search_logs_json, search_logs_json, {}]
+    mock_api_caller._call_api.side_effect = [search_logs_json, {}]
 
     # Test default pagination
     search_log_request = SearchLogRequest.views_by_guid(
@@ -72,12 +72,12 @@ def test_search_log_pagination(mock_logger, mock_api_caller, search_logs_json):
     ]
 
     _assert_search_log_results(response, search_logs_json, expected_sorts)
-    assert mock_api_caller._call_api.call_count == 3
+    assert mock_api_caller._call_api.call_count == 2
     assert mock_logger.call_count == 0
-    mock_api_caller.reset_mock()
+    mock_api_caller._call_api.reset_mock()
 
     # Test bulk pagination
-    mock_api_caller._call_api.side_effect = [search_logs_json, search_logs_json, {}]
+    mock_api_caller._call_api.side_effect = [search_logs_json, {}]
     response = client.search(criteria=search_log_request, bulk=True)
     expected_sorts = [
         SortItem(field="createdAt", order=SortOrder.ASCENDING),
@@ -99,14 +99,13 @@ def test_search_log_pagination(mock_logger, mock_api_caller, search_logs_json):
         in mock_logger.call_args_list[0][0][0]
     )
     mock_logger.reset_mock()
-    mock_api_caller.reset_mock()
+    mock_api_caller._call_api.reset_mock()
 
     # Test automatic bulk search conversion when exceeding threshold
     with patch.object(SearchLogResults, "_MASS_EXTRACT_THRESHOLD", -1):
         mock_api_caller._call_api.side_effect = [
             # Extra call to re-fetch the first page
             # results with updated timestamp sorting
-            search_logs_json,
             search_logs_json,
             search_logs_json,
             {},
@@ -126,7 +125,11 @@ def test_search_log_pagination(mock_logger, mock_api_caller, search_logs_json):
             "Result size (%s) exceeds threshold (%s)"
             in mock_logger.call_args_list[0][0][0]
         )
+    mock_logger.reset_mock()
+    mock_api_caller._call_api.reset_mock()
 
+    with patch.object(SearchLogResults, "_MASS_EXTRACT_THRESHOLD", -1):
+        mock_api_caller._call_api.side_effect = [search_logs_json]
         # Test exception for bulk=False with user-defined sorting and results exceeding the threshold
         search_log_request = SearchLogRequest.views_by_guid(
             guid="some-guid",
@@ -144,24 +147,28 @@ def test_search_log_pagination(mock_logger, mock_api_caller, search_logs_json):
             ),
         ):
             client.search(criteria=search_log_request, bulk=False)
-
-        # Test exception for bulk=True with user-defined sorting
-        search_log_request = SearchLogRequest.views_by_guid(
-            guid="some-guid",
-            size=1,
-            sort=[SortItem(field="some-sort2", order=SortOrder.ASCENDING)],
-            exclude_users=["atlansupport"],
-        )
-        with pytest.raises(
-            InvalidRequestError,
-            match=(
-                "ATLAN-PYTHON-400-067 Unable to execute "
-                "search log bulk search with user-defined sorting options. "
-                "Suggestion: Please ensure that no sorting options are "
-                "included in your search log search request when performing a bulk search."
-            ),
-        ):
-            client.search(criteria=search_log_request, bulk=True)
+            assert mock_api_caller._call_api.call_count == 1
 
     mock_logger.reset_mock()
-    mock_api_caller.reset_mock()
+    mock_api_caller._call_api.reset_mock()
+    # Test exception for bulk=True with user-defined sorting
+    search_log_request = SearchLogRequest.views_by_guid(
+        guid="some-guid",
+        size=1,
+        sort=[SortItem(field="some-sort2", order=SortOrder.ASCENDING)],
+        exclude_users=["atlansupport"],
+    )
+    with pytest.raises(
+        InvalidRequestError,
+        match=(
+            "ATLAN-PYTHON-400-067 Unable to execute "
+            "search log bulk search with user-defined sorting options. "
+            "Suggestion: Please ensure that no sorting options are "
+            "included in your search log search request when performing a bulk search."
+        ),
+    ):
+        client.search(criteria=search_log_request, bulk=True)
+        assert mock_api_caller._call_api.call_count == 0
+
+    mock_logger.reset_mock()
+    mock_api_caller._call_api.reset_mock()
